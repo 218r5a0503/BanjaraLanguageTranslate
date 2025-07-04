c595ee07f4ba35a0b8f94d5897eb00a19419760e
Chawan Vinod <113181217+218r5a0503@users.noreply.github.com>
2025-07-05 02:26:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import load_model, Model
 from tensorflow.keras.layers import Input, LSTM, Dense
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -96,4 +97,5 @@ def translate():
     return jsonify({"banjara": banjara_text})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
